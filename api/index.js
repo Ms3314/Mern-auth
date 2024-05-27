fe18c71ad0a6e71ce9fb35e411d156308ade65a9
Samiuddin <mdsamiuddin2005@outlook.com>
2024-05-27 14:47:43 +0530

    created an server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+const app = express()
+
+app.listen(5000, () => {
+    console.log('server is running on port 5000')
+})
+
+app.get("/" , (req, res) => {
+    res.send("hello world")
+})
